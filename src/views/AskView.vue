739fb87a4ad5e56ad5b9623c32c57ce7558e0aa4
seongjin.ma <seongjin.ma@appg.co.kr>
2025-12-22 14:21:51 +0900

    Feat(중간 정리): 중간 정리 및 스타일링

--- src/views/AskView.vue
+++ src/views/AskView.vue
@@ -1,6 +1,9 @@
 <template>
   <div>
-    <div v-for="item in askItems" :key="item.id">{{ item }}</div>
+    <p v-for="item in this.$store.state.ask" :key="item.id">
+      <a :href="item.url">{{ item.title }}</a>
+      <small>{{ item.time_ago }} by {{ item.user }}</small>
+    </p>
   </div>
 </template>
 
