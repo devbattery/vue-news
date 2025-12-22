d6ad714a6c9f793282f919957126762a2907d8d3
seongjin.ma <seongjin.ma@appg.co.kr>
2025-12-22 16:12:09 +0900

    Refactor(리스트 아이템 컴포넌트 공통화): 질문, 구직 리스트 스타일링

--- src/views/AskView.vue
+++ src/views/AskView.vue
@@ -1,7 +1,27 @@
 <template>
   <div>
+    <ul class="news-list">
+      <li v-for="item in this.$store.state.ask" :key="item.id" class="post">
+        <!-- 포인트 영역 -->
+        <div class="points">
+          {{ item.points }}
+        </div>
+        <!-- 기타 정보 영역 -->
+        <div>
+          <p class="news-title">
+            <router-link :to="`/item/${item.id}`">{{ item.title }}</router-link>
+          </p>
+          <small class="link-text">
+            {{ item.time_ago }} by
+            <router-link class="link-text" :to="`/user/${item.user}`">{{
+              item.user
+            }}</router-link>
+          </small>
+        </div>
+      </li>
+    </ul>
+
     <p v-for="item in askItems" :key="item.id">
-      <!-- <a :href="item.url">{{ item.title }}</a> -->
       <router-link :to="`item/${item.id}`">{{ item.title }}</router-link>
       <small>{{ item.time_ago }} by {{ item.user }}</small>
     </p>
@@ -35,4 +55,26 @@ export default {
 };
 </script>
 
-<style lang="scss" scoped></style>
+<style scoped>
+.news-list {
+  margin: 0%;
+  padding: 0%;
+}
+.post {
+  list-style: none;
+  display: flex;
+  align-items: center;
+  border-bottom: 1px solid #eee;
+}
+.points {
+  width: 80px;
+  height: 60px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  color: #41b883;
+}
+.link-text {
+  color: #828282;
+}
+</style>
--- src/views/JobsView.vue
+++ src/views/JobsView.vue
@@ -1,9 +1,31 @@
 <template>
   <div>
-    <p v-for="item in this.$store.state.jobs" :key="item.id">
-      <a :href="item.url">{{ item.title }}</a>
-      <small>{{ item.time_ago }} by {{ item.domain }}</small>
-    </p>
+    <ul class="news-list">
+      <li v-for="item in this.$store.state.jobs" :key="item.id" class="post">
+        <!-- 포인트 영역 -->
+        <div class="points">
+          {{ item.points || 0 }}
+        </div>
+        <!-- 기타 정보 영역 -->
+        <div>
+          <p class="news-title">
+            <a :href="item.url">{{ item.title }}</a>
+          </p>
+          <small class="link-text">
+            {{ item.time_ago }} by
+            <!-- <router-link class="link-text" :to="`/user/${item.user}`">{{
+              item.user
+            }}</router-link> -->
+            <a :href="item.url">{{ item.domain }}</a>
+          </small>
+        </div>
+      </li>
+    </ul>
+
+    <!-- <p v-for="item in this.$store.state.jobs" :key="item.id">
+      <router-link :to="`item/${item.id}`">{{ item.title }}</router-link>
+      <small>{{ item.time_ago }} by {{ item.user }}</small>
+    </p> -->
   </div>
 </template>
 
@@ -15,4 +37,26 @@ export default {
 };
 </script>
 
-<style lang="scss" scoped></style>
+<style scoped>
+.news-list {
+  margin: 0%;
+  padding: 0%;
+}
+.post {
+  list-style: none;
+  display: flex;
+  align-items: center;
+  border-bottom: 1px solid #eee;
+}
+.points {
+  width: 80px;
+  height: 60px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  color: #41b883;
+}
+.link-text {
+  color: #828282;
+}
+</style>
