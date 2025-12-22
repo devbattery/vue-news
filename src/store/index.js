63b37960a4a5eba280deabffe9566f183a25da40
seongjin.ma <seongjin.ma@appg.co.kr>
2025-12-22 14:11:51 +0900

    Feat(스토어 구현): 스토어 속성 모듈화

--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,7 @@
 import Vue from "vue";
 import Vuex from "vuex";
-import { fetchAskList, fetchJobsList, fetchNewsList } from "@/api";
+import mutations from "./mutations";
+import actions from "./actions";
 
 Vue.use(Vuex);
 
@@ -15,34 +16,6 @@ export const store = new Vuex.Store({
       return state.ask;
     },
   },
-  mutations: {
-    SET_NEWS(state, news) {
-      // news: response.data
-      state.news = news;
-    },
-    SET_JOBS(state, jobs) {
-      state.jobs = jobs;
-    },
-    SET_ASK(state, ask) {
-      state.ask = ask;
-    },
-  },
-  actions: {
-    FETCH_NEWS(context) {
-      fetchNewsList()
-        .then((response) => {
-          console.log(response.data);
-          context.commit("SET_NEWS", response.data);
-        })
-        .catch((error) => console.log(error));
-    },
-    FETCH_JOBS({ commit }) {
-      fetchJobsList()
-        .then(({ data }) => commit("SET_JOBS", data))
-        .catch((error) => console.log(error));
-    },
-    FETCH_ASK({ commit }) {
-      fetchAskList().then(({ data }) => commit("SET_ASK", data));
-    },
-  },
+  mutations,
+  actions,
 });
