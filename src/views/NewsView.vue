293b836e5b1f9be2cc6af001d07169cbfae1d85e
seongjin.ma <seongjin.ma@appg.co.kr>
2025-12-22 13:51:03 +0900

    Feat(스토어 구현): NewsView에 actions와 mutations 적용

--- src/views/NewsView.vue
+++ src/views/NewsView.vue
@@ -1,11 +1,13 @@
 <template>
   <div>
-    <div v-for="user in users" :key="user.id">{{ user }}</div>
+    <div v-for="user in this.$store.state.news" :key="user.id">
+      {{ user.title }}
+    </div>
   </div>
 </template>
 
 <script>
-import { fetchNewsList } from "@/api";
+// import { fetchNewsList } from "@/api";
 export default {
   data() {
     return {
@@ -13,13 +15,12 @@ export default {
     };
   },
   created() {
-    console.log("호출 전: ", this);
-    fetchNewsList()
-      .then((response) => {
-        console.log("호출 후: ", this);
-        this.users = response.data;
-      })
-      .catch((error) => console.log(error));
+    this.$store.dispatch("FETCH_NEWS");
+    // fetchNewsList()
+    //   .then((response) => {
+    //     this.users = response.data;
+    //   })
+    //   .catch((error) => console.log(error));
   },
 };
 </script>
