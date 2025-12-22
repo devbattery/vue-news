34ef612fd2697ce260abdabe8a199c9646c4414a
seongjin.ma <seongjin.ma@appg.co.kr>
2025-12-22 11:33:24 +0900

    Feat(API 구현): axios를 이용한 api 호출

--- src/views/NewsView.vue
+++ src/views/NewsView.vue
@@ -1,9 +1,28 @@
 <template>
-  <div>news</div>
+  <div>
+    <div v-for="user in users" :key="user.id">{{ user }}</div>
+  </div>
 </template>
 
 <script>
-export default {};
+import axios from "axios";
+
+export default {
+  data() {
+    return {
+      users: [],
+    };
+  },
+  created() {
+    axios
+      .get("https://api.hnpwa.com/v0/news/1.json")
+      .then((response) => {
+        console.log(response);
+        this.users = response.data;
+      })
+      .catch((error) => console.log(error));
+  },
+};
 </script>
 
 <style lang="scss" scoped></style>
